3df4b7af93dbee9ab86fbab352b7ca4c1732a55f
lucaskirschner <kirschnerlucas1@gmail.com>
2025-08-28 20:49:37 -0300

    Primera versión de GPIO con patrón ADT

---
+++ src/main.c
@@ -0,0 +1,67 @@
+/************************************************************************************************
+Copyright (c) 2025, Lucas Kirschner <kirschnerlucas1@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/** @file main.c
+ ** @brief Definición de la función principal del programa
+ **
+ ** @addtogroup app_main
+ ** @{ */
+
+/* === Headers files inclusions =============================================================== */
+#include <stdbool.h>
+#include "main.h"
+#include "gpio.h"
+#include "hal_gpio.h"   // Incluye los prototipos de la HAL utilizada
+
+/* === Macros definitions ====================================================================== */
+#define LED_ROJO_PORT   1
+#define LED_ROJO_BIT    7
+
+/* === Private data type declarations ========================================================== */
+
+/* === Private variable declarations =========================================================== */
+
+/* === Private function declarations =========================================================== */
+
+/* === Public variable definitions ============================================================= */
+
+/* === Private variable definitions ============================================================ */
+
+/* === Private function implementation ========================================================= */
+
+/* === Public function implementation ========================================================== */
+
+int main(void) {
+
+    // Inicialización de periféricos
+
+    while(true){
+        gpio_t red_led = gpioCreate(LED_ROJO_PORT, LED_ROJO_BIT);
+        gpioSetOutput(red_led, true);
+        gpioSetState(red_led, false);
+    }
+
+    return 0;
+}
+
+/* === End of documentation ==================================================================== */
+
+/** @} end of addtogroup app_main */
